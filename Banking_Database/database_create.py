6f171cf1ab965c5b9d2e53dae4ebeaa345905712
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-23 14:36:52 +0200

    create_connection function in database created and updated

--- Banking_Database/database_create.py
+++ Banking_Database/database_create.py
@@ -6,3 +6,13 @@ database_path = './Banking_Database/banking.db'
 def create_connection():
 
     """ creates and returns a connection to the SQLite database """
+    connection = None
+
+    try:
+        connection = sqlite3.connect(database_path)
+        return connection
+
+    except sqlite3.Error as e:
+        print(f"Error connecting to database: {e}")
+
+    return connection
